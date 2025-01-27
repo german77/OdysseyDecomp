20863efc2c50863e5b27503eab3e7259c8ae75a8
german77 <5944268+german77@users.noreply.github.com>
2025-01-27 12:52:32 -0600

    Item: Implement CoinCollectHolder

--- src/Item/CoinCollectHolder.h
+++ src/Item/CoinCollectHolder.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <basis/seadTypes.h>
+#include <container/seadPtrArray.h>
 #include <math/seadVector.h>
 
 #include "Library/Scene/ISceneObj.h"
@@ -29,6 +30,11 @@ public:
     CoinCollect2D* tryFindAliveCoinCollect2D(const sead::Vector3f&, bool) const;
     CoinCollect2D* tryFindAliveCoinCollect2D(const sead::Vector3f&, f32, f32, bool) const;
     CoinCollectHintObj* tryFindExStageHintObjTrans(sead::Vector3f*, const char*);
+
+private:
+    sead::PtrArray<CoinCollect> mCollect;
+    sead::PtrArray<CoinCollect2D> mCollect2D;
+    sead::PtrArray<CoinCollectHintObj> mCollectHintObj;
 };
 
 namespace rs {
