97e3130c809bb0d79366c3e854095352f3e17e1d
german77 <5944268+german77@users.noreply.github.com>
2025-04-24 11:13:27 -0600

    Sequence: Implement E3Sequence

--- lib/al/Library/Layout/LayoutUtil.h
+++ lib/al/Library/Layout/LayoutUtil.h
@@ -7,8 +7,19 @@ class RenderBuffer;
 
 namespace al {
 class LayoutKit;
+class LayoutInitInfo;
+class SceneObjHolder;
+class AudioDirector;
+class LayoutSystem;
+class MessageSystem;
+class GamePadSystem;
 
+void initLayoutInitInfo(LayoutInitInfo*, const LayoutKit*, SceneObjHolder*, const AudioDirector*,
+                        const LayoutSystem*, const MessageSystem*, const GamePadSystem*);
 void setRenderBuffer(LayoutKit*, const agl::RenderBuffer*);
+void executeUpdate(LayoutKit*);
+void executeUpdateList(LayoutKit*, const char*, const char*);
+void executeUpdateEffect(LayoutKit*);
 void executeDraw(const LayoutKit*, const char*);
 
 }  // namespace al
