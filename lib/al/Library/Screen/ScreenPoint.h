f90bbc012bef124873cbdfbce07641ca12b6eb57
german77 <5944268+german77@users.noreply.github.com>
2025-10-31 10:42:36 -0600

    Library/Screen: Implement ScreenPointer

--- lib/al/Library/Screen/ScreenPoint.h
+++ lib/al/Library/Screen/ScreenPoint.h
@@ -1,9 +1,45 @@
 #pragma once
 
-namespace al {
-class LiveActor;
-}  // namespace al
+#include <basis/seadTypes.h>
+#include <container/seadObjArray.h>
+#include <math/seadVector.h>
 
-namespace alScreenPointFunction {
-void updateScreenPointAll(al::LiveActor* actor);
-}  // namespace alScreenPointFunction
+namespace al {
+struct ActorInitInfo;
+class ScreenPointDirector;
+class ScreenPointTarget;
+
+struct ScreenPointTargetHitInfo {
+    void* _0;
+    ScreenPointTarget* target;
+    float screenPoint;
+    float directPoint;
+    sead::Vector3f aa;
+    sead::Vector3f bb;
+};
+
+static_assert(sizeof(ScreenPointTargetHitInfo) == 0x30);
+
+class ScreenPointer {
+public:
+    ScreenPointer(const ActorInitInfo& info, const char* name);
+
+    bool hitCheckSegment(const sead::Vector3f&, const sead::Vector3f&);
+    bool hitCheckScreenCircle(const sead::Vector2f&, f32, f32);
+    bool hitCheckLayoutCircle(const sead::Vector2f&, f32, f32,
+                              s32 (*)(const ScreenPointTargetHitInfo*,
+                                      const ScreenPointTargetHitInfo*));
+    bool recheckAndSortSegment(const sead::Vector3f&, const sead::Vector3f&);
+    ScreenPointTarget* getHitTarget(s32) const;
+    bool isHitTarget(const ScreenPointTarget* target) const;
+
+    s32 getHitTargetNum() const { return targetHitInfo.size(); }
+
+private:
+    ScreenPointDirector* mDirector = nullptr;
+    sead::ObjArray<ScreenPointTargetHitInfo> targetHitInfo;
+};
+
+static_assert(sizeof(ScreenPointer) == 0x28);
+
+}  // namespace al
--- lib/al/Library/Screen/ScreenPointDirector.h
+++ lib/al/Library/Screen/ScreenPointDirector.h
@@ -7,18 +7,21 @@ namespace al {
 class ScreenPointer;
 class ScreenPointCheckGroup;
 class ScreenPointTarget;
-class ScreenPointTargetHitInfo;
+struct ScreenPointTargetHitInfo;
 
 class ScreenPointDirector {
 public:
     ScreenPointDirector();
     void registerTarget(ScreenPointTarget*);
     void setCheckGroup(ScreenPointTarget*);
-    bool hitCheckSegment(ScreenPointer*, sead::ObjArray<ScreenPointTargetHitInfo>*, s32,
-                         const sead::Vector3f&, const sead::Vector3f&);
-    bool hitCheckScreenCircle(ScreenPointer*, sead::ObjArray<ScreenPointTargetHitInfo>*, s32,
+    bool hitCheckSegment(ScreenPointer* screenPointer,
+                         sead::ObjArray<ScreenPointTargetHitInfo>* targetHitInfo, s32 size,
+                         const sead::Vector3f& a, const sead::Vector3f& b);
+    bool hitCheckScreenCircle(ScreenPointer* screenPointer,
+                              sead::ObjArray<ScreenPointTargetHitInfo>* targetHitInfo, s32 size,
                               const sead::Vector2f&, f32, f32);
-    bool hitCheckLayoutCircle(ScreenPointer*, sead::ObjArray<ScreenPointTargetHitInfo>*, s32,
+    bool hitCheckLayoutCircle(ScreenPointer* screenPointer,
+                              sead::ObjArray<ScreenPointTargetHitInfo>* targetHitInfo, s32 size,
                               const sead::Vector2f&, f32, f32,
                               s32 (*)(const ScreenPointTargetHitInfo*,
                                       const ScreenPointTargetHitInfo*));
