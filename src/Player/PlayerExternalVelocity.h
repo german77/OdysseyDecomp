c608196adcca4cee2268822b5ac200ad07532ce9
german77 <5944268+german77@users.noreply.github.com>
2025-11-21 13:38:46 -0600

    Player: Implement PlayerExternalVelocity

--- src/Player/PlayerExternalVelocity.h
+++ src/Player/PlayerExternalVelocity.h
@@ -2,6 +2,22 @@
 
 class PlayerExternalVelocity {
 public:
+    void PlayerExternalVelocity(const al::LiveActor*, const IUsePlayerCollision*,
+                                const sead::Vector3<float>*);
+    void receiveMsgPlayer(const al::SensorMsg*, al::HitSensor*, al::HitSensor*);
+    void receiveMsgCap(const al::SensorMsg*, al::HitSensor*, al::HitSensor*);
+    void requestApplyLastGroundInertia();
+    void cancelAndFeedbackLastGroundInertia(al::LiveActor*, float, bool);
+    void update();
+    void updatePadRumbleExternalForce();
+    void updateLastGroundForce(bool);
+    void reset();
     bool isExistForce() const;
     bool isExistSnapForce() const;
+
+private:
+    al::ExternalForceKeeper mExternalForceKeeper;
+
 };
+
+static_assert(sizeof(Bird) == 0x98);
