04eb27c916167a984be05740c768f395e13781e1
german77 <5944268+german77@users.noreply.github.com>
2025-12-13 18:42:59 -0600

    Hitsegment

--- lib/al/Library/Math/MathUtil.cpp
+++ lib/al/Library/Math/MathUtil.cpp
@@ -1759,6 +1759,72 @@ const char* axisIndexToString(s32 axisIndex) {
     }
 }
 
+bool checkHitSegmentSphere(const sead::Vector3f& start, const sead::Vector3f& end,
+                           const sead::Vector3f& center, f32 radius, sead::Vector3f* outDir,
+                           sead::Vector3f* outPos) {
+    sead::Vector3f segDir = start - end;
+    sead::Vector3f endToCenter = center - end;
+
+    f32 dot = segDir.dot(endToCenter);
+    f32 radiusSqr = radius * radius;
+
+    if (dot < 0.0f) {
+        if ((end - start).squaredLength() < radiusSqr) {
+            tryNormalizeOrZero(&segDir);
+
+            if (outDir != nullptr)
+                outDir->set(segDir);
+            if (outPos != nullptr)
+                outPos->set(start - segDir * radius);
+            return true;
+        }
+
+        return false;
+    }
+
+    if (endToCenter.squaredLength() < dot) {
+        if ((center - start).squaredLength() < radiusSqr) {
+            segDir = start - center;
+            tryNormalizeOrZero(&segDir);
+
+            if (outDir != nullptr)
+                outDir->set(segDir);
+            if (outPos != nullptr)
+                outPos->set(start - segDir * radius);
+            return true;
+        }
+
+        return false;
+    }
+
+    if (isNearZero(endToCenter.squaredLength())) {
+        if (segDir.squaredLength() <= radiusSqr) {
+            tryNormalizeOrZero(&segDir);
+
+            if (outDir != nullptr)
+                outDir->set(segDir);
+            if (outPos != nullptr)
+                outPos->set(start - segDir * radius);
+            return true;
+        }
+        return false;
+    }
+
+    segDir = endToCenter * (dot / endToCenter.squaredLength()) - segDir;
+
+    if (segDir.squaredLength() <= radiusSqr) {
+        segDir = -segDir;
+        tryNormalizeOrZero(&segDir);
+
+        if (outDir != nullptr)
+            outDir->set(segDir);
+        if (outPos != nullptr)
+            outPos->set(start - segDir * radius);
+        return true;
+    }
+    return false;
+}
+
 }  // namespace al
 
 namespace Intersect {
