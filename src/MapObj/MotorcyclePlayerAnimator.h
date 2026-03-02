8a2f7459919ccf73a11e55ad7dfaccf9f750cdfc
Narr the Reg <5944268+german77@users.noreply.github.com>
2026-03-02 14:10:25 -0600

    motor

---
+++ src/MapObj/MotorcyclePlayerAnimator.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#include "Library/Nerve/NerveExecutor.h"
+
+class IUsePlayerPuppet;
+
+class MotorcyclePlayerAnimator : public al::NerveExecutor {
+public:
+    MotorcyclePlayerAnimator();
+
+    void update(f32, f32, f32);
+    void startBind(IUsePlayerPuppet*);
+    void startBindKeepDemo();
+    void endBind();
+    void startBindRideStartL();
+    void startBindRideStartR();
+    void startBindRideOn();
+    void startBindWait();
+    void startBindRideRunStart();
+    void startBindRideClash();
+    void startBindRideJump();
+    void tryStartBindRideRunIfNotPlaying();
+    void tryStartBindRideLandIfJump();
+    void exeNone();
+    void exeRideStartL();
+    void exeRideStartR();
+    void exeRideOn();
+    void exeWait();
+    void exeRideRunStart();
+    void exeRideRun();
+    void exeRideClash();
+    void exeRideJump();
+    void exeRideLand();
+    void exeBindKeepDemo();
+
+private:
+    IUsePlayerPuppet* mPlayerPuppet;
+    f32 floatA;
+    f32 floatB;
+    f32 floatC;
+};
+
+static_assert(sizeof(MotorcyclePlayerAnimator) == 0x28);
