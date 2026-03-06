e6a8de577e37603256fd4bd0dbe197496ae5e350
Narr the Reg <5944268+german77@users.noreply.github.com>
2026-03-06 09:52:39 -0600

    Library/Se: Implement SeDemoSyncedProcInfo and copyAudioInfoList

--- lib/al/Project/Audio/AudioEffectProcInfo.h
+++ lib/al/Project/Audio/AudioEffectProcInfo.h
@@ -1,19 +1,19 @@
 #pragma once
 
-//#include <basis/seadTypes.h>
+#include <container/seadPtrArray.h>
 
 namespace al {
 
 struct AudioDemoSyncedProcInfo {
     AudioDemoSyncedProcInfo();
+    AudioDemoSyncedProcInfo(const AudioDemoSyncedProcInfo& info);
 
     const char* name = nullptr;
     bool isEnableEvent = false;
     bool isPartsProc = false;
-    void* _10 = nullptr;
+    sead::PtrArray<const char>* list = nullptr;
 };
 
 static_assert(sizeof(AudioDemoSyncedProcInfo) == 0x18);
-
 
 }  // namespace al
